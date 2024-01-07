ec2593047bf139b54a1f97602ad08ca125321f8e
Rahad-Ullah <Rahad-Ullah@users.noreply.github.com>
2024-01-07 12:31:34 +0600

    Designed Middle section of Navbar

--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -1,20 +1,42 @@
-import { Bell, Menu, Upload, User } from "lucide-react";
+import { Bell, Menu, MicIcon, Search, Upload, User } from "lucide-react";
 import logo from "../assets/Logo.png"
 import Button from "../components/Button";
+import { useState } from "react";
 
 const PageHeader = () => {
+    const [showFullWidthSearch, setShowFullWidthSearch] = useState(false)
+    
     return (
         <div className="flex justify-between items-center gap-10 lg:gap-20 py-2 px-3">
             <div className="flex items-center gap-4">
                 <Button variant={"ghost"} size={"icon"}>
                     <Menu/>
                 </Button>
-                <a href="/">
+                <a href="/" className="">
                     <img src={logo} alt="logo" className="h-6"/>
                 </a>
             </div>
-            <div></div>
+            
+            {/* Middle section */}
+            <form className="md:flex hidden justify-center items-center gap-4 flex-grow">
+                <div className="flex flex-grow max-w-screen-sm">
+                    <input type="search" placeholder="Search" className="border rounded-l-full border-secondary-border shadow-inner shadow-secondary py-1 px-4 text-lg w-full focus:border-blue-500 outline-none"/>
+                    <Button className="py-2 px-4 rounded-r-full border border-secondary-border border-l-0">
+                        <Search/>
+                    </Button>
+                </div>
+                <Button type="button" size={"icon"}>
+                    <MicIcon/>
+                </Button>
+            </form>
+            {/* Right section */}
             <div className="flex flex-shrink-0 md:gap-2">
+                <Button variant={"ghost"} size={"icon"} className="md:hidden">
+                    <Search/>
+                </Button>
+                <Button variant={"ghost"} size={"icon"} className="md:hidden">
+                    <MicIcon/>
+                </Button>
                 <Button variant={"ghost"} size={"icon"}>
                     <Upload/>
                 </Button>
