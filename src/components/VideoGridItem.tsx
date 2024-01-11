6aa406e7f12a1a8afb87382dac107a3159f9f387
Rahad-Ullah <Rahad-Ullah@users.noreply.github.com>
2024-01-11 17:08:59 +0600

    Channel image, video title and views has been showed

--- src/components/VideoGridItem.tsx
+++ src/components/VideoGridItem.tsx
@@ -1,3 +1,5 @@
+import formatDuration from "../utils/formatDuration";
+
 type VideoGridItemProps = {
   id: string;
   title: string;
@@ -13,6 +15,8 @@ type VideoGridItemProps = {
   videoUrl: string;
 };
 
+const VIEW_FORMATTER = new Intl.NumberFormat(undefined, {notation: "compact"})
+
 const VideoGridItem = ({
   id,
   title,
@@ -26,10 +30,26 @@ const VideoGridItem = ({
   return <div className="flex flex-col gap-2">
     <a href={`/watch?v=${id}`} className="relative aspect-video">
         <img src={thumbnailUrl} className="block w-full h-full object-cover rounded-xl" />
-        <div className="absolute bottom-1 right-1 bg-secondary-dark text-sm px-0.5 rounded">
-            {duration}
+        <div className="absolute bottom-1 right-1 bg-secondary-dark text-white text-sm px-0.5 rounded">
+            {formatDuration(duration)}
         </div>
     </a>
+    <div className="flex gap-2">
+        <a href={`/@${channel.id}`} className="flex-shrink-0">
+            <img src={channel.profileUrl} alt="channel" className="w-12 h-12 rounded-full" />
+        </a>
+        <div className="flex flex-col">
+            <a href={`/watch?v=${id}`} className="font-bold">
+                {title}
+            </a>
+            <a href={`/@${channel.id}`} className="text-secondary-text text-sm">
+                {channel.name}
+            </a>
+            <div className="text-secondary-text text-sm">
+                {VIEW_FORMATTER.format(views)} Views ● 
+            </div>
+        </div>
+    </div>
   </div>;
 };
 
