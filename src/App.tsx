66dc1f676b99d76bbc4fbead9115633971de6859
Rahad-Ullah <Rahad-Ullah@users.noreply.github.com>
2024-01-11 12:20:48 +0600

    Video Item has been showed on UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,29 @@
-import CategoryPills from "./components/CategoryPills"
-import { categories } from "./components/data/categories"
-import PageHeader from "./layouts/PageHeader"
-
-
+import CategoryPills from "./components/CategoryPills";
+import VideoGridItem from "./components/VideoGridItem";
+import { categories } from "./components/data/categories";
+import { videos } from "./components/data/videos";
+import PageHeader from "./layouts/PageHeader";
 
 function App() {
-
   return (
     <div>
       {/* <h1 className='text-4xl text-center my-10 font-semibold'>WebTube</h1> */}
-      <PageHeader/>
+      <PageHeader />
       <div className="grid grid-cols-[auto,1fr] flex-grow overflow-auto">
-        <div>
-            Sidebar
-        </div>
+        <div>Sidebar</div>
         <div className="overflow-x-hidden px-8 pb-4">
           <div className="sticky top-0 bg-white z-10 pb-4">
-          <CategoryPills categories={categories}/>
+            <CategoryPills categories={categories} />
+          </div>
+          <div className="grid gap-4 grid-cols-[repeat(auto-fill,minmax(300px,1fr))]">
+            {videos.map((video) => (
+              <VideoGridItem key={video.id} {...video} />
+            ))}
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
